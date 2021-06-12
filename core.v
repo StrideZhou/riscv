2c7f72117ae4188366467c05749a634317b69c06
stride <stride_anderson@qq.com>
2021-06-12 15:14:39 +0800

    devide pip stage 1 & 2

--- core.v
+++ core.v
@@ -16,6 +16,7 @@ module core (
     output   [31:0]  inst
 );
 
+//* pip stage 1  ---------------------------------------
 //ins_mod
     wire        br_en = 0;
     wire [32:0] br_addr = 0;
@@ -39,38 +40,50 @@ ins_mod ins_mod(
     .exIns_ren   ( exIns_ren   ),
     .exIns_addr  ( exIns_addr  )
 );
+syn_reg#(.W ( 32 ))       pc_reg1( clk,nrst,1,pc,pc_r1 );
+
+//* pip stage 2  ---------------------------------------
+inst_decoder inst_decoder1(
+    .inst        ( inst        ),
+    .inst_rs1    ( radd1       ),
+    .inst_rs2    ( radd2       )
+//    .mem_opcode  ( mem_opcode  )
+);
 
 regFile regFile(
     .nrst  ( nrst  ),
     .clk   ( clk   ),
     .radd1 ( radd1 ),
     .radd2 ( radd2 ),
-    .wen   ( wen   ),
-    .wadd  ( wadd  ),
-    .wdata ( wdata ),
-    .rs1   ( rs1   ),
-    .rs2   ( rs2   )
+    .wen   ( rf_wen   ),
+    .wadd  ( rf_wadd  ),
+    .wdata ( rf_wdata ),
+    .rs1   ( rs1_data ),
+    .rs2   ( rs2_data )
 );
-syn_reg#(.W ( 32 ))     u_syn_reg( clk,nrst,1,next,value );
 
+syn_reg#(.W ( 32 ))       pc_reg2( clk,nrst,1, pc_r1, pc_r2   );
+syn_reg#(.W ( 32 ))     inst_reg2( clk,nrst,1, inst , inst_r2 );
+
+//* pip stage 3  ---------------------------------------
 
 alu_mod alu_mod(
     .inst     ( inst     ),
     .rs1_data ( rs1_data ),
     .rs2_data ( rs2_data ),
-    .rd_data  ( rd_data  ),
+    .rd_data  ( alu_rd_data  ),
     .pc       ( pc       ),
     .br_en    ( br_en_x  )//! x preliminary
 );
 
 memory dmem(
-    .clk     ( clk     ),
-    .nrst    ( nrst    ),
-    .stall   ( stall   ),
-    .op_code ( op_code ),
-    .rwaddr  ( rwaddr  ),
-    .wdata   ( wdata   ),
-    .rdata   ( rdata   )
+    .clk     ( clk        ),
+    .nrst    ( nrst       ),
+    .stall   ( 1'b0       ),
+    .op_code ( mem_opcode ),
+    .rwaddr  ( alu_rd_data    ),
+    .wdata   ( mem_wdata  ),
+    .rdata   ( mem_rdata  )
 );
 
 
