901b5a5e40a547a9f7e5802158c5fa29ff415365
stride <stride_anderson@qq.com>
2021-06-17 18:53:59 +0800

    fix blocking & noblocking

--- regFile.v
+++ regFile.v
@@ -1,6 +1,4 @@
-module regFile #(
-    parameter W = 32, N = 32 //Width = 32, Number of regs = 32
-)(
+module regFile(
     input           nrst,   //asynchronous reset, low effective
     input           clk,
 
@@ -9,18 +7,47 @@ module regFile #(
 
     input              wen,    //write enable
     input      [4:0]   wadd,
-    input      [W-1:0] wdata,
+    input      [31:0] wdata,
 
-    output reg [W-1:0] rs1,    //output register values
-    output reg [W-1:0] rs2
+    output reg [31:0] rs1,    //output register values
+    output reg [31:0] rs2
 );
 
-reg [W-1:0] reg_file [1:N-1]; // the register file
+reg [31:0] reg_file [1:31]; // the register file
 
-integer i;
 always @(posedge clk or negedge nrst) begin 
     if (~nrst) begin
-            for (i = 1; i < N; i = i + 1) reg_file[i] = 32'b0;
+        reg_file[1] <= 32'b0;
+        reg_file[2] <= 32'b0;
+        reg_file[3] <= 32'b0;
+        reg_file[4] <= 32'b0;
+        reg_file[5] <= 32'b0;
+        reg_file[6] <= 32'b0;
+        reg_file[7] <= 32'b0;
+        reg_file[8] <= 32'b0;
+        reg_file[9] <= 32'b0;
+        reg_file[10] <= 32'b0;
+        reg_file[11] <= 32'b0;
+        reg_file[12] <= 32'b0;
+        reg_file[13] <= 32'b0;
+        reg_file[14] <= 32'b0;
+        reg_file[15] <= 32'b0;
+        reg_file[16] <= 32'b0;
+        reg_file[17] <= 32'b0;
+        reg_file[18] <= 32'b0;
+        reg_file[19] <= 32'b0;
+        reg_file[20] <= 32'b0;
+        reg_file[21] <= 32'b0;
+        reg_file[22] <= 32'b0;
+        reg_file[23] <= 32'b0;
+        reg_file[24] <= 32'b0;
+        reg_file[25] <= 32'b0;
+        reg_file[26] <= 32'b0;
+        reg_file[27] <= 32'b0;
+        reg_file[28] <= 32'b0;
+        reg_file[29] <= 32'b0;
+        reg_file[30] <= 32'b0;
+        reg_file[31] <= 32'b0;
     end
     else begin
         if (wen && (wadd != 5'b0)) begin // not write to reg0
