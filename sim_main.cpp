a6817a1b1ab0f552ceaca400b4f556b35fdef475
stride <stride_anderson@qq.com>
2023-02-16 17:44:36 +0800

    fit verilator

---
+++ sim_main.cpp
@@ -0,0 +1,105 @@
+#include <memory>
+#include <verilated.h>
+#include "verilated_vcd_c.h" //可选，如果要导出wave则需要加上
+#include "Vtop_tb.h"    // Verilog模块会被编译成Vxxx
+
+int main(int argc, char **argv, char **env){
+
+    // Construct a VerilatedContext to hold simulation time, etc.
+    // Multiple modules (made later below with Vtop) may share the same
+    // context to share time, or modules may have different contexts if
+    // they should be independent from each other.
+
+    // Using unique_ptr is similar to
+    // "VerilatedContext* contextp = new VerilatedContext" then deleting at end.
+    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+
+    // Set debug level, 0 is off, 9 is highest presently used
+    // May be overridden by commandArgs argument parsing
+    contextp->debug(0);
+
+    // Randomization reset policy
+    // May be overridden by commandArgs argument parsing
+    contextp->randReset(2);
+
+
+    // Pass arguments so Verilated code can see them, e.g. $value$plusargs
+    // This needs to be called before you create any model
+    contextp->commandArgs(argc, argv);
+    
+    const std::unique_ptr<Vtop_tb> top{new Vtop_tb{contextp.get(), "top_tb"}};
+
+#if VM_TRACE
+    VerilatedVcdC* tfp = nullptr;
+    const char* flag = contextp->commandArgsPlusMatch("trace");
+    if (flag && 0 == strcmp(flag, "+trace")) {
+        contextp->traceEverOn(true);
+        VL_PRINTF("Enabling waves into wave.vcd...\n");
+        tfp = new VerilatedVcdC;
+        top->trace(tfp, 99);
+        tfp->open("wave.vcd");
+    }
+
+    // VerilatedFstC* tfp = nullptr;
+    // const char* flag = contextp->commandArgsPlusMatch("trace");
+    // if (flag && 0 == strcmp(flag, "+trace")) {
+    //     contextp->traceEverOn(true);
+    //     VL_PRINTF("Enabling waves into wave.fst...\n");
+    //     tfp = new VerilatedFstC;
+    //     top->trace(tfp, 99);
+    //     tfp->open("wave.fst");
+    // }
+
+
+#endif
+    int sim_time = 10000;
+    top->clk = 0;
+    // Simulate until $finish
+    while (contextp->time() < sim_time && !contextp->gotFinish()) { 
+        // Historical note, before Verilator 4.200 Verilated::gotFinish()
+        // was used above in place of contextp->gotFinish().
+        // Most of the contextp-> calls can use Verilated:: calls instead;
+        // the Verilated:: versions simply assume there's a single context
+        // being used (per thread).  It's faster and clearer to use the
+        // newer contextp-> versions.
+
+        contextp->timeInc(1);  // 1 timeprecision period passes...
+        // Historical note, before Verilator 4.200 a sc_time_stamp()
+        // function was required instead of using timeInc.  Once timeInc()
+        // is called (with non-zero), the Verilated libraries assume the
+        // new API, and sc_time_stamp() will no longer work.
+
+        top->clk = ~top->clk & 0x1;
+        //? VL_PRINTF("[%" PRId64 "] vlt running...\n", contextp->time());
+
+        if (!top->clk) {
+            if (contextp->time() > 1 && contextp->time() < 10) {
+                top->rst = 1;  // Assert reset
+            } else {
+                top->rst = 0;  // Deassert reset
+            }
+        }
+
+        // Evaluate model
+        // (If you have multiple models being simulated in the same
+        // timestep then instead of eval(), call eval_step() on each, then
+        // eval_end_step() on each. See the manual.)
+        top->eval(); 
+#if VM_TRACE
+        if (tfp) tfp->dump(contextp->time());
+#endif
+
+    }
+
+    top->final();
+
+    // Close trace if opened
+#if VM_TRACE
+    if (tfp) {
+        tfp->close();
+        tfp = nullptr;
+    }
+#endif
+
+    return 0;
+}
--- top_tb.sv
+++ top_tb.sv
@@ -1,12 +1,15 @@
-module test;
-    reg clk;
-    reg nrst;
 
-    reg  [31:0] inst;
-    reg  [31:0] pc;
+module top_tb (
+    input clk,
+    input rst
+);
+    reg nrst = ~rst;
+
+    reg  [31:0] inst = 0;
+    reg  [31:0] pc = 0;
 
     reg         exIns_valid = 0;
-    reg  [31:0] exIns_in = 32'hx;
+    reg  [31:0] exIns_in = 32'h0;
 
     wire        exIns_ren;
     wire [31:0] exIns_addr;
@@ -14,34 +17,43 @@ module test;
     /* Init clk and nrst */
     initial begin
         nrst = 1'b1;
-        #0 nrst = 1'b0;
-        #1 nrst = 1'b1;
+    //     #0 nrst = 1'b0;
+    //     #1 nrst = 1'b1;
     end
 
+    // initial begin
+    //     clk = 1'b1;
+    //     forever begin
+    //         #1 clk = ~clk;
+    //     end
+    // end
+
+    integer i,j;
     initial begin
-        clk = 1'b1;
-        forever begin
-            #1 clk = ~clk;
-        end
-    end
-    integer i;
-    initial begin
+
         $readmemh( "riscv.hex", u_core.ins_mod.imem.mem1.mem );
-        
         for (i=0; i<256; i=i+1) begin
             u_core.dmem_mod.dmem.mem1.mem[i] = 32'b0;
             u_core.dmem_mod.dmem.mem2.mem[i] = 32'b0;
         end
 
-        #530 $stop; 
+        // #530 $finish; 
     end
 
     always @(posedge clk ) begin
-        if (pc == 32'h0000_0328) 
-        // if (pc == 32'h0000_0174) 
-        // if (pc == 32'h0000_00fc) 
-        #7 $stop;
+        if (pc == 32'h0000_0328 || pc == 32'h0000_0174 || pc == 32'h0000_00fc)begin 
+            $display("============================");
+            for (i=0; i<8; i=i+1) begin
+                $write("@%H:\t", i*4 );
+                for (j=0; j<4; j=j+1) begin
+                    $write("%H ",u_core.regFile.reg_file[ i*4 + j ]);
+                end
+                $write("\n");
+            end
+        end
+        // #7 $finish;
     end
+
 core u_core(
     .clk          ( clk         ),
     .nrst         ( nrst        ),
